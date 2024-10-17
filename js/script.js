e07169abd90e46a509bb46ac78596d95d80bbe77
SkedaddlesAway <abraxoszz@gmail.com>
2024-10-17 16:58:55 -0500

    Filling things in

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function validateForm(){
+    let fname = document.forms["signupForm"]["firstName"].value;
+    let lname = document.forms["signupForm"]["lastName"].value;
+    let email = document.forms["signupForm"]["email"].value;
+    if (fname == "" || lname == "" || email == "") {
+      alert("All fields must be filled out!");
+      return false;
+    } 
+    return true;
+  } 
